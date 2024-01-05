00597677078777d780a0be258b5d982d56fe062a
Vincent Theis <vinctilus@gmail.com>
2024-01-05 22:26:00 +0000

    :pencil2: Fix typos in Round

--- Assets/Skrips/round.cs
+++ Assets/Skrips/round.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class round : MonoBehaviour
+public class Round : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < transform.childCount; i++) {
